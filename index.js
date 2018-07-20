4fdf2be9467d37e7225d1a50321e6f92f302d97a
walk8243 <walk8243@gmail.com>
2018-07-21 04:06:34 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ server.on('request', (req, res) => {
   res.end(fs.readFileSync("./views/index.html"));
 });
 
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', socket => {
+var io = require('socket.io')(server);
+io.on('connection', socket => {
+  socket.on('disconnect', () => {
+    console.log("disconnected! " + socket.id);
+  });
+
   socket.emit('greeting', {message: 'hello'}, data => {
-    console.log('result: ' + data);
+    console.log(socket.id + ' result: ' + data);
   });
   socket.on('request', msg => {
     console.log(msg);
